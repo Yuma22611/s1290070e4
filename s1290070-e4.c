789d8deab135a9cbd7665675f1c74cc94d854511
Yuma22611 <108879864+Yuma22611@users.noreply.github.com>
2022-07-09 17:34:55 +0900

    Victory message added

--- s1290070-e4.c
+++ s1290070-e4.c
@@ -22,5 +22,8 @@ int main() {
     }
     printf("Heads: %d, Tails: %d\n", cnthead, cnttails);
 
+    if(cnthead > cnttails) printf("You won!\n");
+    else printf("You lost.\n");
+
     return 0;
 }
